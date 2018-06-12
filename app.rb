d63d25322f4cc8976db43dd33bb74d5ed1939f28
meredith winchell <meredithwinchell@gmail.com>
2018-06-12 20:52:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class GameApp < Sinatra::Base
   get '/rps/:play' do
-    @requests = params
+  
     # Don't worry about this UNTIL you've passed all the tests!
 
     erb :rps_game
